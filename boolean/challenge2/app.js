a73eccf6e05c9722af39978aae1d54117cef5f72
hasbi-alaziz <94413730+hasbi-alajiz@users.noreply.github.com>
2025-11-15 11:05:40 +0700

    penyelesaian soal

---
+++ boolean/challenge2/app.js
@@ -0,0 +1,7 @@
+const isTall = false; // short asf LMAO
+const canCook = false; // bruh, i couldnt even cook, more like im cooked lmao
+const hasSiblings = true;
+
+console.log(isTall);
+console.log(canCook);
+console.log(hasSiblings);
