aeaf9e7e6a575325bde6f4b9b3e317ab75a19c41
hasbi-alaziz <94413730+hasbi-alajiz@users.noreply.github.com>
2025-11-15 11:31:32 +0700

    penyelesaian soal

---
+++ conditional/challenge1/app.js
@@ -0,0 +1,7 @@
+const hasPassport = true;
+
+if (hasPassport) {
+    console.log('kamu punya akses untuk pergi ke luar negeri');
+} else {
+    console.log('kamu tidak punya akses untuk pergi ke luar negeri');
+}
