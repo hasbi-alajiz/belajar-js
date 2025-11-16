ef41c3a1580882907f5bffc8aa28ad594ea0fc70
hasbi-alaziz <94413730+hasbi-alajiz@users.noreply.github.com>
2025-11-16 12:17:48 +0700

    penyelesaian soal

---
+++ array/challenge1/app.js
@@ -0,0 +1,13 @@
+// var input = prompt('masukkan nama:');
+
+const userNames = [
+    'yandy',
+    'dimas',
+    'mita',
+];
+var newUsername;
+if (newUsername = userNames.includes('anjay')) {
+    console.log('username sudah digunakan');
+} else {
+    console.log('username tersedia');
+}
